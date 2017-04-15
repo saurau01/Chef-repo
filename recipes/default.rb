4afdeabf4f89c5aec894c2fecb3196018cd12692
System Administrator <root@SAURABHs-MacBook-Pro.local>
2017-04-15 17:41:07 +0530

    new tomcat recepie

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+package 'java-1.7.0-openjdk-devel'
+
+group 'tomcat'
+
+user 'tomcat' do
+     manage_home false
+     shell '/bin/nologin'
+     group 'tomcat'
+     home '/opt/tomcat'
+end
+
+
+
+# end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,36 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
+#unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
+ # describe user('root'), :skip do
+  #  it { should exist }
+ # end
+#end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+#describe port(8080), do
+ # it { should_not be_listening }
+#end
+
+describe command('curl localhost:8080') do
+  its('stdout') { should match (/Tomcat/) }
+end
+describe package ('java-1.7.0-openjdk-devel') do
+  it { should be_installed }
+end
+describe file ('/opt/tomcat') do
+  it {should be_directory}
+  it { should be_owned_by 'tomcat'}
+end
+end
+describe user ('tomcat') do
+  it {should exist}
+  its ('group') {should eq 'tomcat'}
+  its ('home') {should eq '/opt/tomcat'}
+  its ('shell') {should eq '/bin/nologin'}
+
+end
+describe group('tomcat') do
+  it {should exist}
 end
